c72b8064e3f16de357e42d43b64a2fdcc2f376f0
kyrylo_kundik <goliathusua@gmail.com>
2020-05-21 11:50:07 +0300

    Model polishing and improvements

--- app/System/MVC/Model.php
+++ app/System/MVC/Model.php
@@ -11,7 +11,7 @@ abstract class Model
     private DatabaseAdapter $conn;
     protected $tablename;
 
-    public final function __construct($conn)
+    public final function __construct()
     {
         if (!isset($this->tablename))
             trigger_error(get_class($this) . ' must have a $tablename');
@@ -26,8 +26,65 @@ abstract class Model
         );
     }
 
-    /**
-     * @return mixed
-     */
-    abstract public function getTablename();
+    public function findAll()
+    {
+        return $this->conn->query("SELECT * FROM $this->tablename");
+    }
+
+    public function findById($id)
+    {
+        $id = $this->conn->escape($id);
+        $res = $this->conn->query("SELECT * FROM $this->tablename WHERE id = $id");
+        if (empty($res)) {
+            throw new \Exception("Record not found");
+        }
+        return $res[0];
+    }
+
+    public function update($fields, $values, $id = null)
+    {
+        assert(count($fields) == count($values), 'Fields and Values must be same size');
+        $fields = array_map(array(&$this, "escapeFunction"), $fields);
+        $values = array_map(array(&$this, "escapeFunction"), $values);
+
+        $stmt = "UPDATE $this->tablename SET ";
+        foreach ($fields as $key => $field) {
+            $value = $values[$key];
+            $stmt .= "$field = $value";
+
+            if ($key < count($fields) - 1)
+                $stmt .= ", ";
+        }
+        if (!is_null($id)) {
+            $id = $this->conn->escape($id);
+            $stmt .= " WHERE id=$id";
+        }
+        $this->conn->query($stmt);
+    }
+
+    public function delete($id = null)
+    {
+        $stmt = "DELETE FROM $this->tablename";
+        if (!is_null($id)) {
+            $id = $this->conn->escape($id);
+            $stmt .= " WHERE id=$id";
+        }
+        $this->conn->query($stmt);
+    }
+
+    public function insert($fields, $values)
+    {
+        assert(count($fields) == count($values), 'Fields and Values must be same size');
+        $fields = array_map(array(&$this, "escapeFunction"), $fields);
+        $values = array_map(array(&$this, "escapeFunction"), $values);
+
+        $stmt = "INSERT INTO $this->tablename(" . join(", ", $fields) . ") VALUES (" . join(",", $values) . ")";
+        $this->conn->query($stmt);
+        return $this->conn->getLastId();
+    }
+
+    private function escapeFunction($value)
+    {
+        return $this->conn->escape($value);
+    }
 }
