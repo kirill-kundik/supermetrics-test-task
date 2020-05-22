527d9124bba24d3903640b4c7b97d5d2b0a84d88
kyrylo_kundik <goliathusua@gmail.com>
2020-05-22 13:16:10 +0300

    config improvements

--- app/config.php
+++ app/config.php
@@ -5,11 +5,13 @@ $scriptName = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
 define('HTTP_URL', '/' . substr_replace(trim($_SERVER['REQUEST_URI'], '/'), '', 0, strlen($scriptName)));
 
 // Define Path Application
-define('SCRIPT', str_replace('\\', '/', rtrim(__DIR__ . "/../", '/')) . '/');
+$appFolder = getenv("APP_FOLDER");
+
+define('SCRIPT', str_replace('\\', '/', rtrim(__DIR__, '/')) . '/');
 define('SYSTEM', SCRIPT . 'System/');
-define('CONTROLLERS', SCRIPT . 'Application/Controllers/');
-define('MODELS', SCRIPT . 'Application/Models/');
-define('VIEWS', SCRIPT . 'Application/Views/');
+define('CONTROLLERS', SCRIPT . $appFolder . '/Controllers/');
+define('MODELS', SCRIPT . $appFolder . '/Models/');
+define('VIEWS', SCRIPT . $appFolder . '/Views/');
 
 // Config Database
 define('DATABASE', [
